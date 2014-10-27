87b3266c73b1cdecbb1402eca56bb87b7e1a2b49
Florian Görlitz <florian.goerlitz@conlance.eu>
2014-10-27 14:16:04 +0100

    moment-js with formatted date

--- Brocfile.js
+++ Brocfile.js
@@ -19,5 +19,6 @@ app.import('vendor/fontello/font/fontello.woff', {
 });
 
 app.import('bower_components/picnic/releases/latest.min.css');
+app.import('bower_components/moment/moment.js');
 
 module.exports = app.toTree();
